dc5ff1f66503a5a32516816450ea02223bbe1360
GarvIIITD <163018011+GarvIIITD@users.noreply.github.com>
2024-11-25 17:43:40 +0530

    Update Trajectory.java

--- angrybird'/core/src/main/java/com/angry/bird/Trajectory.java
+++ angrybird'/core/src/main/java/com/angry/bird/Trajectory.java
@@ -1,8 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+package com.angry.bird;
+
+import com.badlogic.gdx.math.Vector2;
 
 public class Trajectory {
-    public List<Vector> calculatePath(Vector initialVelocity, float gravity) {
-        return new ArrayList<>();
+    public static final float GRAVITY = -9.8f; // Gravitational constant
+
+    private Vector2 startPosition;
+    private Vector2 velocity;
+
+    public Trajectory(Vector2 startPosition, Vector2 velocity) {
+        this.startPosition = startPosition;
+        this.velocity = velocity;
+    }
+
+    // Simulate trajectory for a given time
+    public Vector2 predictPosition(float time) {
+        float x = startPosition.x + velocity.x * time;
+        float y = startPosition.y + velocity.y * time + 0.5f * GRAVITY * time * time;
+        return new Vector2(x, y);
     }
 }
