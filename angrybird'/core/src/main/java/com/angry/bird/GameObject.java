0ab05ac3c80ee0a42ed1df5e61623d75f3fb84f6
GarvIIITD <163018011+GarvIIITD@users.noreply.github.com>
2024-11-25 17:34:25 +0530

    Update GameObject.java

--- angrybird'/core/src/main/java/com/angry/bird/GameObject.java
+++ angrybird'/core/src/main/java/com/angry/bird/GameObject.java
@@ -1,7 +1,40 @@
-public abstract class GameObject {
-    protected Vector position;
-    protected Vector size;
+package com.angry.bird;
 
-    public abstract void update();
-    public abstract void render();
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class GameObject {
+    protected Vector2 position;
+
+    public GameObject(float x, float y) {
+        this.position = new Vector2(x, y);
+    }
+
+    public float getX() {
+        return position.x;
+    }
+
+    public void setX(float x) {
+        this.position.x = x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public void setY(float y) {
+        this.position.y = y;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public abstract void render(SpriteBatch batch);
+
+    public abstract void dispose();
 }
