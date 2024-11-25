40aa80d5fcd7cb01f71f05686150888adc0bdf81
GarvIIITD <163018011+GarvIIITD@users.noreply.github.com>
2024-11-25 17:40:03 +0530

    Update MediumPig.java

--- angrybird'/core/src/main/java/com/angry/bird/MediumPig.java
+++ angrybird'/core/src/main/java/com/angry/bird/MediumPig.java
@@ -1,11 +1,12 @@
+package com.angry.bird;
+
 public class MediumPig extends Pig {
+    public MediumPig(float x, float y, int hitsToDestroy, String texturePath) {
+        super(x, y, hitsToDestroy, texturePath);
+    }
+
     @Override
     public void takeHit(int damage) {
-    }
-    @Override
-    public void update() {
-    }
-    @Override
-    public void render() {
+
     }
 }
