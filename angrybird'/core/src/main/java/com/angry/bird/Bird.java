04cf57a7fc6d3de3cd9215e68686eb00ddd8bdb0
GarvIIITD <163018011+GarvIIITD@users.noreply.github.com>
2024-11-25 17:30:48 +0530

    Update Bird.java

--- angrybird'/core/src/main/java/com/angry/bird/Bird.java
+++ angrybird'/core/src/main/java/com/angry/bird/Bird.java
@@ -1,15 +1,47 @@
-public abstract class Bird extends GameObject {
-    private String color;
-    private int damage;
-    private float speed;
-    protected SpecialAbility specialAbility;
+package com.angry.bird;
 
-    public void fire() {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class Bird {
+    protected Vector2 position;
+    protected Texture texture;
+    protected Vector2 velocity;
+
+    public Bird(float x, float y, String texturePath) {
+        this.position = new Vector2(x, y);
+        this.velocity = new Vector2(0, 0);
+        this.texture = new Texture(texturePath);
     }
 
-    public Trajectory calculateTrajectory() {
-        return new Trajectory();
+    public void render(SpriteBatch batch, float width, float height) {
+        batch.draw(texture, position.x, position.y, width, height);
     }
-    public abstract void activateSpecialAbility();
+
+    public boolean checkCollision(Pig pig) {
+        return false;
+    }
+
+
+    public void update(float delta) {
+        position.add(velocity.x * delta, velocity.y * delta);
+        velocity.y -= 9.8f * delta; // Simulate gravity
+    }
+
+    public void fire(float angle, float power) {
+        velocity.x = (float) (power * Math.cos(Math.toRadians(angle)));
+        velocity.y = (float) (power * Math.sin(Math.toRadians(angle)));
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public void render(SpriteBatch batch) {
+    }
 }
-
