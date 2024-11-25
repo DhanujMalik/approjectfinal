6c5dae7d78a7905e733ab6b6e0e76391e7e62dc8
GarvIIITD <163018011+GarvIIITD@users.noreply.github.com>
2024-11-25 17:42:30 +0530

    Update RedBird.java

--- angrybird'/core/src/main/java/com/angry/bird/RedBird.java
+++ angrybird'/core/src/main/java/com/angry/bird/RedBird.java
@@ -1,11 +1,13 @@
+package com.angry.bird;
+
 public class RedBird extends Bird {
-    @Override
-    public void activateSpecialAbility() {
+    public RedBird(float x, float y) {
+        super(x, y, "red_bird.png"); // Path to Red Bird texture
     }
+
     @Override
-    public void update() {
-    }
-    @Override
-    public void render() {
+    public void fire(float angle, float power) {
+        super.fire(angle, power);
+        // Add any unique behavior for RedBird here
     }
 }
