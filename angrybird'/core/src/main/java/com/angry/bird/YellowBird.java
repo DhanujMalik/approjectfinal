5285c5da949c0fd282261d226dba759d4dc18e3b
GarvIIITD <163018011+GarvIIITD@users.noreply.github.com>
2024-11-25 17:44:16 +0530

    Update YellowBird.java

--- angrybird'/core/src/main/java/com/angry/bird/YellowBird.java
+++ angrybird'/core/src/main/java/com/angry/bird/YellowBird.java
@@ -1,11 +1,13 @@
+package com.angry.bird;
+
 public class YellowBird extends Bird {
-    @Override
-    public void activateSpecialAbility() {
+    public YellowBird(float x, float y) {
+        super(x, y, "yellow_bird.png"); // Path to Yellow Bird texture
     }
+
     @Override
-    public void update() {
-    }
-    @Override
-    public void render() {
+    public void fire(float angle, float power) {
+        super.fire(angle, power);
+        // Add any unique behavior for YellowBird here
     }
 }
