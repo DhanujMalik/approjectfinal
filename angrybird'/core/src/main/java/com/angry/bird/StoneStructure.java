582c889c543b3f1f89097a95121c2414521fda24
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:35:18 +0530

    Update StoneStructure.java

--- angrybird'/core/src/main/java/com/angry/bird/StoneStructure.java
+++ angrybird'/core/src/main/java/com/angry/bird/StoneStructure.java
@@ -1,11 +1,9 @@
-public class StoneStructure extends Structure {
-    @Override
-    public void takeHit(int damage) {
-    }
-    @Override
-    public void update() {
-    }
-    @Override
-    public void render() {
+package com.angry.bird;
+
+import com.angry.bird.Structure;
+
+class StoneStructure extends Structure {
+    public StoneStructure(float x, float y) {
+        super(x, y, "stone_structure.png", 3); // Strong durability
     }
 }
