b5ebdc389664cb8f083914f574916d9b102aa012
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:35:52 +0530

    Update WoodStructure.java

--- angrybird'/core/src/main/java/com/angry/bird/WoodStructure.java
+++ angrybird'/core/src/main/java/com/angry/bird/WoodStructure.java
@@ -1,11 +1,9 @@
-public class WoodStructure extends Structure {
-    @Override
-    public void takeHit(int damage) {
-    }
-    @Override
-    public void update() {
-    }
-    @Override
-    public void render() {
+package com.angry.bird;
+
+import com.angry.bird.Structure;
+
+class WoodenStructure extends Structure {
+    public WoodenStructure(float x, float y) {
+        super(x, y, "wooden_structure.png", 2); // Medium durability
     }
 }
