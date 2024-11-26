eccc836100acc8aeb684fa613ade26eda22de5c3
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:36:12 +0530

    Update YellowBird.java

--- angrybird'/core/src/main/java/com/angry/bird/YellowBird.java
+++ angrybird'/core/src/main/java/com/angry/bird/YellowBird.java
@@ -1,13 +1,23 @@
 package com.angry.bird;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class YellowBird extends Bird {
-    public YellowBird(float x, float y) {
-        super(x, y, "yellow_bird.png"); // Path to Yellow Bird texture
+    private static final float WIDTH = 20.0f; // Width in pixels
+    private static final float HEIGHT = 20.0f; // Height in pixels
+    private static final String TEXTURE_PATH = "red_bird.png";
+
+    public YellowBird(World world, float x, float y) {
+        super(world, x, y, TEXTURE_PATH, WIDTH, HEIGHT);
     }
 
     @Override
-    public void fire(float angle, float power) {
-        super.fire(angle, power);
-        // Add any unique behavior for YellowBird here
+    public float getWidth() {
+        return WIDTH;
+    }
+
+    @Override
+    public float getHeight() {
+        return HEIGHT;
     }
 }
