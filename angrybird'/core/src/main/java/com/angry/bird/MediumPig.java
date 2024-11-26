9a22c270e01fda37b81b098cf231f133c124e421
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:32:29 +0530

    Update MediumPig.java

--- angrybird'/core/src/main/java/com/angry/bird/MediumPig.java
+++ angrybird'/core/src/main/java/com/angry/bird/MediumPig.java
@@ -1,12 +1,15 @@
 package com.angry.bird;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class MediumPig extends Pig {
-    public MediumPig(float x, float y, int hitsToDestroy, String texturePath) {
-        super(x, y, hitsToDestroy, texturePath);
-    }
 
-    @Override
-    public void takeHit(int damage) {
+    private static final int HITS_TO_DESTROY = 2;
+    private static final String TEXTURE_PATH = "medium_pig.png";
+    private static final float WIDTH = 50; // Screen units
+    private static final float HEIGHT = 50; // Screen units
 
+    public MediumPig(World world, float x, float y) {
+        super(world, x, y, HITS_TO_DESTROY, TEXTURE_PATH, WIDTH, HEIGHT);
     }
 }
