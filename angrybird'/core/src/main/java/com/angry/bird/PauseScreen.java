394fd39175b8dd85828ab3d63dad36531260c268
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:33:51 +0530

    Update PauseScreen.java

--- angrybird'/core/src/main/java/com/angry/bird/PauseScreen.java
+++ angrybird'/core/src/main/java/com/angry/bird/PauseScreen.java
@@ -1,13 +1,14 @@
-
 package com.angry.bird;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -15,12 +16,15 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class PauseScreen implements Screen {
 
     private final Main game;
+    private final EasyLevel currentGameScreen;
     private final Stage stage;
+    private final SpriteBatch batch;
     private Texture background;
-    private SpriteBatch batch;
+    private boolean isSoundMuted;
 
-    public PauseScreen(final Main game) {
+    public PauseScreen(Main game, EasyLevel currentGameScreen) {
         this.game = game;
+        this.currentGameScreen = currentGameScreen;
         this.stage = new Stage(new ScreenViewport());
         this.batch = new SpriteBatch();
 
@@ -28,38 +32,57 @@ public class PauseScreen implements Screen {
 
         Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 
+        // Resume Button
         TextButton resumeButton = new TextButton("Resume", skin);
-        resumeButton.setSize(200, 80); 
-        resumeButton.setPosition(Gdx.graphics.getWidth() / 2f - 100, Gdx.graphics.getHeight() / 2f + 50); 
+        resumeButton.setSize(200, 80);
+        resumeButton.setPosition(Gdx.graphics.getWidth() / 2f - 100, Gdx.graphics.getHeight() / 2f + 50);
         resumeButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game));
+                game.setScreen(currentGameScreen); // Resume the existing game screen
             }
         });
 
+        // Save and Exit Button
         TextButton saveExitButton = new TextButton("Save and Exit", skin);
-        saveExitButton.setSize(200, 80); 
-        saveExitButton.setPosition(Gdx.graphics.getWidth() / 2f - 100, Gdx.graphics.getHeight() / 2f - 100); 
+        saveExitButton.setSize(200, 80);
+        saveExitButton.setPosition(Gdx.graphics.getWidth() / 2f - 100, Gdx.graphics.getHeight() / 2f - 100);
         saveExitButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new MainPage(game)); 
+                // Handle saving game state if necessary
+                game.setScreen(new MainPage(game)); // Return to MainPage
             }
         });
 
+        // Sound Toggle Button
+        TextButton soundToggleButton = new TextButton("Sound: ON", skin);
+        soundToggleButton.setSize(200, 80);
+        soundToggleButton.setPosition(Gdx.graphics.getWidth() / 2f - 100, Gdx.graphics.getHeight() / 2f - 25);
+        soundToggleButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                isSoundMuted = !isSoundMuted;
+                soundToggleButton.setText("Sound: " + (isSoundMuted ? "OFF" : "ON"));
+                // Add logic to mute/unmute game sounds
+            }
+        });
+
+        // Add buttons to the stage
         stage.addActor(resumeButton);
         stage.addActor(saveExitButton);
+        stage.addActor(soundToggleButton);
 
         Gdx.input.setInputProcessor(stage);
     }
 
     @Override
-    public void show() {
-    }
+    public void show() {}
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
@@ -74,16 +97,13 @@ public class PauseScreen implements Screen {
     }
 
     @Override
-    public void pause() {
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
