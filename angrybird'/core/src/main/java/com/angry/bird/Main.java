2f94e7e28896b8503e198c2736669b2efa366f7b
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:31:26 +0530

    Update Main.java

--- angrybird'/core/src/main/java/com/angry/bird/Main.java
+++ angrybird'/core/src/main/java/com/angry/bird/Main.java
@@ -1,24 +1,42 @@
 package com.angry.bird;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends Game {
+    // Create a Music object
+    private Music music;  // Remove the instantiation here
+
     public SpriteBatch batch;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        this.setScreen(new Splash(this)); 
+
+        // Initialize the music object here
+        music = Gdx.audio.newMusic(Gdx.files.internal("path/to/your/music.wav"));  // Load the music file
+
+        // Play background music
+        music.setLooping(true);  // Make it loop continuously
+        music.play();
+
+        // You can also play a sound effect here if needed (use another method for sound effects)
+        // music.playSoundEffect("path/to/sound_effect.wav");
+
+        this.setScreen(new Splash(this));
     }
 
     @Override
     public void render() {
-        super.render(); 
+        super.render();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+        music.dispose();  // Dispose of the music when you're done
     }
 }
+
