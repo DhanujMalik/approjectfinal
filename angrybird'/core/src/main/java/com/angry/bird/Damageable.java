19d450b85cd71ac9e70ec90e6fea397a4eecb8e7
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:26:38 +0530

    Update Damageable.java

--- angrybird'/core/src/main/java/com/angry/bird/Damageable.java
+++ angrybird'/core/src/main/java/com/angry/bird/Damageable.java
@@ -1,3 +1,18 @@
+package com.angry.bird;
+
 public interface Damageable {
-    void takeHit(int damage);
+    /**
+     * Inflicts damage on the object.
+     *
+     * @param damage The amount of damage to inflict.
+     */
+    void takeDamage(int damage);
+
+    /**
+     * Checks if the object is destroyed.
+     *
+     * @return True if the object is destroyed, false otherwise.
+     */
+    boolean isDestroyed();
 }
+
