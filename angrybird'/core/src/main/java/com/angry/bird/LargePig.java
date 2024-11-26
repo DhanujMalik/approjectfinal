a9b16885d1071fdafb0c05f2fa5514ce68fd3b89
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:29:51 +0530

    Update LargePig.java

--- angrybird'/core/src/main/java/com/angry/bird/LargePig.java
+++ angrybird'/core/src/main/java/com/angry/bird/LargePig.java
@@ -1,12 +1,15 @@
 package com.angry.bird;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class LargePig extends Pig {
-    public LargePig(float x, float y, int hitsToDestroy, String texturePath) {
-        super(x, y, hitsToDestroy, texturePath);
-    }
 
-    @Override
-    public void takeHit(int damage) {
+    private static final int HITS_TO_DESTROY = 3;
+    private static final String TEXTURE_PATH = "large_pig.png";
+    private static final float WIDTH = 60; // Screen units
+    private static final float HEIGHT = 60; // Screen units
 
+    public LargePig(World world, float x, float y) {
+        super(world, x, y, HITS_TO_DESTROY, TEXTURE_PATH, WIDTH, HEIGHT);
     }
 }
