4b358571692e804a194414d953d91466d055aead
DhanujMalik <163024578+DhanujMalik@users.noreply.github.com>
2024-11-27 02:34:46 +0530

    Update RedBird.java

--- angrybird'/core/src/main/java/com/angry/bird/RedBird.java
+++ angrybird'/core/src/main/java/com/angry/bird/RedBird.java
@@ -1,13 +1,23 @@
 package com.angry.bird;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class RedBird extends Bird {
-    public RedBird(float x, float y) {
-        super(x, y, "red_bird.png"); // Path to Red Bird texture
+    private static final float WIDTH = 20.0f; // Width in pixels
+    private static final float HEIGHT = 20.0f; // Height in pixels
+    private static final String TEXTURE_PATH = "red_bird.png";
+
+    public RedBird(World world, float x, float y) {
+        super(world, x, y, TEXTURE_PATH, WIDTH, HEIGHT);
     }
 
     @Override
-    public void fire(float angle, float power) {
-        super.fire(angle, power);
-        // Add any unique behavior for RedBird here
+    public float getWidth() {
+        return WIDTH;
+    }
+
+    @Override
+    public float getHeight() {
+        return HEIGHT;
     }
 }
